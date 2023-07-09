9d63d5c51b59b06d741c29cdd9e40571875d34b8
Pr0pyh <andrej.drmanovic@gmail.com>
2023-07-09 14:05:05 +0200

    deveti commit

---
+++ scripts/Levels/Level.cs
@@ -0,0 +1,32 @@
+using Godot;
+using System;
+
+public class Level : Spatial
+{
+    // Declare member variables here. Examples:
+    // private int a = 2;
+    // private string b = "text";
+    [Export]
+    public bool final;
+    AnimationPlayer animPlayer;
+    // Called when the node enters the scene tree for the first time.
+    public override void _Ready()
+    {
+        animPlayer = GetNode<AnimationPlayer>("AnimationPlayer");
+        animPlayer.Play("fadeIn");
+    }
+
+    public void _on_AnimationPlayer_animation_finished(String animName)
+    {
+        if(animName == "fadeIn" && final == true)
+            animPlayer.Play("ending");
+        else if(animName == "ending")
+            GetTree().Quit();
+
+    }
+//  // Called every frame. 'delta' is the elapsed time since the previous frame.
+//  public override void _Process(float delta)
+//  {
+//      
+//  }
+}
